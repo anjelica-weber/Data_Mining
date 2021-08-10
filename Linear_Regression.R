d2e55f3be146a109025adcc0dc8dad58ad5a4038
Weber <anjelica.weber@mssm.edu>
2021-08-10 10:07:23 -0400

    Saving changes

---
+++ Linear_Regression.R
@@ -0,0 +1,10 @@
+
+# Load Libraries ----------------------------------------------------------
+library(caTools)
+library(tidyverse)
+
+# Train / Test Data Split -------------------------------------------------
+#data points for training and testing set selected using a random number generator (RNG)
+#setting seed number for the RNG, this can be any arbitrary number 
+set.seed(101)
+
