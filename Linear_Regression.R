883bb16261bb48e22eec0c1f03ad4a70c5a9cc07
Weber <anjelica.weber@mssm.edu>
2021-08-10 16:47:04 -0400

    Adding in lin regression eval and predictions

--- Linear_Regression.R
+++ Linear_Regression.R
@@ -19,5 +19,12 @@ lr_model <- lm(yi ~ xi, df_lin)
 
 # Evaluating Model --------------------------------------------------------
 model_stats <- summary(lr_model)
+#coefficients used in linear equation to make predictions
 model_stats$coefficients
+#Measure of goodness of fit of model
+#Describes hoe much variation is explained by the model. 1 is the best.
 model_stats$r.squared
+
+# Predictions -------------------------------------------------------------
+predict(lr_model, df_lin)
+
