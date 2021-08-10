08c55cb97027294b6dbefdf5a998845aa73bbe1f
Weber <anjelica.weber@mssm.edu>
2021-08-10 12:02:30 -0400

    Saving changes

--- Linear_Regression.R
+++ Linear_Regression.R
@@ -8,3 +8,18 @@ library(tidyverse)
 #setting seed number for the RNG, this can be any arbitrary number 
 set.seed(101)
 
+#Split ratio is 70% for training 30% testing
+#function creates boolean vector T for 70% of data F for 30%
+#pass in any column from data set
+sample <- sample.split(df$age, SplitRatio = 0.7)
+
+train <- subset(df, sample == T)
+test <- subset(df, sample == F)
+
+# Training Model ----------------------------------------------------------
+#equation y ~ x1 + x2 + etc, or y ~. for all variables
+lr_model <- lm(target ~., train)
+
+#display linear equation used for predictions
+summary(lr_model)
+
