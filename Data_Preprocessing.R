61f1894ad1a4279b49af3704f1df5427299fdb33
Weber <anjelica.weber@mssm.edu>
2021-08-11 15:15:20 -0400

    Updating comments

--- Data_Preprocessing.R
+++ Data_Preprocessing.R
@@ -10,7 +10,15 @@ library(xlsx)
 #Option 1 - Remove missing values
 
 #Option 2 - Replace missing values
+#Option 2a - Replace with average / most frequent
+
+#Option 2b - Replace with random value
+
 
 # Data Types --------------------------------------------------------------
-#categorical data must be factored
+#Note: Categorical data must be factored
+#Note: Meta data should not be used to train models
+
+# Data Normalization ------------------------------------------------------
+
 
