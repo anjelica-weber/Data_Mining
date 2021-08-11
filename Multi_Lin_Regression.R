638ec1fdad450f1b29456af889022158dfeeffe7
Weber <anjelica.weber@mssm.edu>
2021-08-11 14:02:52 -0400

    Saving changes

--- Multi_Lin_Regression.R
+++ Multi_Lin_Regression.R
@@ -45,7 +45,7 @@ model_stats <- summary(multi_lr_model)
 #coefficients used in linear equation to make predictions
 model_stats$coefficients
 #Measure of goodness of fit of model
-#Describes hoe much variation is explained by the model. 1 is the best.
+#Describes how much variation is explained by the model. 1 is the best.
 model_stats$r.squared
 
 # Predictions -------------------------------------------------------------
