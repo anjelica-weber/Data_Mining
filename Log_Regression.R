c47b0331244e6e663fae4407a324c0a177c4be12
Weber <anjelica.weber@mssm.edu>
2021-08-24 12:58:13 -0400

    Preprocessing log dataset

--- Log_Regression.R
+++ Log_Regression.R
@@ -11,6 +11,23 @@ dir_ex <- paste0("J:/deans/Presidents/SixSigma/Individual Folders",
 df_heart <- read.xlsx2(file = paste0(dir_ex, "/Datasets.xlsx"),
                      sheetName = "HeartFailure")
 
+# Pre-Processing ----------------------------------------------------------
+df_heart <- df_heart %>%
+  mutate(age = as.numeric(age),
+         anaemia = as.factor(anaemia),
+         creatinine_phosphokinase = as.numeric(creatinine_phosphokinase),
+         diabetes = as.factor(diabetes),
+         ejection_fraction = as.numeric(ejection_fraction),
+         high_blood_pressure = as.factor(high_blood_pressure),
+         platelets = as.numeric(platelets),
+         serum_creatinine = as.numeric(serum_creatinine),
+         serum_sodium = as.numeric(serum_sodium),
+         sex = as.factor(sex),
+         smoking = as.factor(smoking),
+         time = as.numeric(time),
+         DEATH_EVENT = as.factor(DEATH_EVENT))
+
+
 # Train / Test Data Split -------------------------------------------------
 #data points for training and testing set selected using a random number generator (RNG)
 #setting seed number for the RNG, this can be any arbitrary number 
@@ -26,7 +43,10 @@ test <- subset(df_heart, sample == F)
 
 # Training Model ----------------------------------------------------------
 #equation y ~ x1 + x2 + etc, or y ~. for all variables
-lr_model <- lm(target ~., train)
+log_model <- glm(DEATH_EVENT ~., train, family = "binomial")
 
 #display linear equation used for predictions
-summary(lr_model)
+summary(log_model)
+
+# Predictions -------------------------------------------------------------
+head(predict(log_model, test))
